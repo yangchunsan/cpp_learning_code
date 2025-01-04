ecbb62096216702285ba27fd002d078195de4e38
HengRui Li <403056885@qq.com>
2025-01-04 23:28:11 +0800

    2025/1/4 commit

---
+++ program/2024.1.4/main.cpp
@@ -0,0 +1,123 @@
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "short:" << sizeof(short) << std::endl;
+//    std::cout << "int:" << sizeof(int) << std::endl;
+//    std::cout << "long:" << sizeof(long int) << std::endl;
+//    std::cout << "long long:" << sizeof(long long int) << std::endl;
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//int main(){
+//    int num = 10;
+//    std::cout << num << std::endl;
+//
+//    int num1;
+//    num1 = 100;
+//    std::cout << num1 << std::endl;
+//
+//    int chineseScore = 90, mathsScore = 100;
+//    std::cout << "chineseScore = " << chineseScore << " mathsScore = " << mathsScore << std::endl;
+//
+//    const int AGE = 18;
+//    std::cout << AGE << std::endl;
+//
+//    float f1 = 3.14;
+//    double f2 = 3.14;
+//    std::cout << f1 << " " << f2 << std::endl;
+//
+//    bool b1 = true;
+//    bool b2 = false;
+//    std::cout << "b1 = " << b1 << " b2 = " << b2 << std::endl;
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//int main(){
+//    char c1 = '\'';
+//    std::string str1 = "hello \" world";
+//
+//    std::cout << c1 << std::endl;
+//    std::cout << str1 <<std::endl;
+//
+//    std::string str2 = "hello \n world";
+//    std::cout << str2 << std::endl;
+//
+//    std::cout << "\\" << std::endl;
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//int main(){
+//    int n = 1;
+//
+//    long long int m = n;
+//    std::cout << m << std::endl;
+//
+//    char ch = 'a';
+//    int ch1 = ch;
+//    std::cout << ch1 << std::endl;
+//
+//    float num = n;
+//    std::cout << num << std::endl;
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//int main(){
+////    int n1 = 0;
+////    std::cout << "please input a number:" << std::endl;
+////    std::cin >> n1;
+////    std::cout << "n1 = " << n1 << std::endl;
+//
+////    float n1 = 0;
+////    std::cout << "please input a number:" << std::endl;
+////    std::cin >> n1;
+////    std::cout << "n1 = " << n1 << std::endl;
+//
+////    bool n1 = 0;
+////    std::cout << "please input a number:" << std::endl;
+////    std::cin >> n1;
+////    std::cout << "n1 = " << n1 << std::endl;
+//
+//    std::string n1 = "";
+//    std::cout << "please input string:" << std::endl;
+//    std::cin >> n1;
+//    std::cout << "n1 = " << n1 << std::endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//#include <limits>
+//
+//int main(){
+//    int num1 = 0;
+//    std::cout << "please input a number" << std::endl;
+//    std::cin >> num1;
+//    std::cout << "num1 = " << num1 << std::endl;
+//    std::cout << "good = " << std::cin.good() << " fail = " << std::cin.fail() << std::endl;
+//    std::cin.clear();
+//    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+//
+//    int num2 = 0;
+//    std::cout << "please input a number" << std::endl;
+//    std::cin >> num2;
+//    std::cout << "num2 = " << num2 << std::endl;
+//
+//    return 0;
+//}
+
+#include <iostream>
+
+int main(){
+    
+}
