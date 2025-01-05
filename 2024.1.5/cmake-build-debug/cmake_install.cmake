1b14a6cc26c971cc41d85fa750da4b5898cdcb64
HengRui Li <403056885@qq.com>
2025-01-05 22:31:23 +0800

    2025/1/5 commit

--- 2024.1.5/cmake-build-debug/cmake_install.cmake
+++ 2024.1.5/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Code/Cpp_Learning_Code/program/2024.1.4
+# Install script for directory: C:/Code/Cpp_Learning_Code/2024.1.5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/2024_1_4")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/2024_1_5")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Code/Cpp_Learning_Code/program/2024.1.4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Code/Cpp_Learning_Code/2024.1.5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
