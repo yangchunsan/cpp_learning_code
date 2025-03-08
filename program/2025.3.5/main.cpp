36e22c2328569c71df94caa811adb4da3d08f6dd
HengRui Li <403056885@qq.com>
2025-03-08 19:39:51 +0800

    2025/3/8 commit

--- program/2025.3.5/main.cpp
+++ program/2025.3.5/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
+class Person{
+public:
+    Person (){
+        cout << "无参构造函数的调用" << endl;
+    }
+
+    ~Person(){
+        cout << "析构函数的调用" << endl;
+    }
+};
+
+void test(){
+    Person P;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Person p;
+//    test();
+
     return 0;
 }
